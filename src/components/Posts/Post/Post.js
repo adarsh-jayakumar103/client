5f8ee26b59c2efcbbbbfc6f82f6894e8285c8496
Adarsh <adarsh.jayakumar@cs.christuniversity.in>
2022-01-13 17:59:14 +0530

    commit 2

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,9 +1,20 @@
 import react from 'react';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import useStyles from './styles';
-const Post= () => {
+
+
+const Post= ({post}) => {
     const classes = useStyles();
     return(
-        <h1>POST</h1>
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
+            <div className={classes.overlay}>
+                <Typography variant= "h6"></Typography>
+            </div>
+        </Card>
     );
 }
 export default Post;
